ad54931b14ee3317abe6c23dcbb716eff6eb49db
Elodie Jance <elodie.jance@gmail.com>
2023-12-22 14:26:28 +0100

    essaie js categorie

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -15,3 +15,21 @@ fetch("http://localhost:5678/api/works")
       gallery.appendChild(figure);
     }
   });
+
+//création d'un nouvel objet avec set
+const categories = new Set();
+//const categoriesArray = Array.from(categories);
+categories.add(categories);
+if (categories.has("hotels & restaurants")) {
+  categories.forEach((categorie) => {
+    //affichage des élément correspondant
+    console.table(categories);
+  });
+}
+/*const boutonFiltrer = document.querySelector(".btn-filtrer");
+
+boutonFiltrer.addEventListener("click", function () {
+  const categoriesFiltrees = categories.filter(function (categorie) {
+    return categorie.ordre <= 12;
+  });
+});*/
