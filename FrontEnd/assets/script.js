6aad3f7e5328d9caca74d824a4eba12fc3b569cd
Elodie Jance <elodie.jance@gmail.com>
2023-12-23 11:55:44 +0100

    essai plusieurs codage pour les filtres gallery 23

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -13,19 +13,57 @@ fetch("http://localhost:5678/api/works")
       figure.appendChild(img);
       figure.appendChild(figcaption);
       gallery.appendChild(figure);
+      // Ajout de la catégorie du travail à l'ensemble categories
+      //categories.add(work.category);
     }
   });
 
-//création d'un nouvel objet avec set
+/*const categories = new Set();
+categories.add("Objets");
+categories.add("Appartements");
+categories.add("hotels & restaurants");
+if (
+  categories.has("Objets") &&
+  categories.has("Appartements") &&
+  categories.has("hotels &amp; restaurants")
+) {
+  categories.forEach((categorie) => {
+    console.log(categorie);
+  });
+}
+function genererCategories(projets) {
+  const gallery = document.querySelector(".gallery");
+  projets.forEach(function (projet) {
+    const projetElement = document.createElement("div");
+    projetElement.textContent = projet;
+    gallery.appendChild(projetElement);
+  });
+}
+const boutonsFiltrer = document.querySelectorAll(".btn-filtrer");
+boutonsFiltrer.forEach(function (bouton) {
+  bouton.addEventListener("click", function () {
+    const categorie = this.id;
+    const projetsFiltres = Array.from(categories).filter(function (
+      categorieProjet
+    ) {
+      return (
+        categorieProjet.trim().toLowerCase() === categorie.trim().toLowerCase()
+      );
+    });
+    document.querySelector(".gallery").innerHTML = "";
+    genererCategories(projetsFiltres);
+  });
+});*/
+/*//création d'un nouvel objet avec set
 const categories = new Set();
 //const categoriesArray = Array.from(categories);
 categories.add(categories);
-if (categories.has("hotels & restaurants")) {
+if (categories.has("Objets", "Appartements", "hotels & restaurants")) {
   categories.forEach((categorie) => {
     //affichage des élément correspondant
     console.table(categories);
   });
-}
+}*/
 /*const boutonFiltrer = document.querySelector(".btn-filtrer");
 
 boutonFiltrer.addEventListener("click", function () {
