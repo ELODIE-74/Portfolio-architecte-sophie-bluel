6be39c0bcdc7242b6fcfb25d747a86e83c5a4331
Elodie Jance <elodie.jance@gmail.com>
2024-01-12 14:48:05 +0100

    modification css + class hidden boutons filtres mode edition le 12/01

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -20,7 +20,7 @@ if (accessToken) {
   const buttonHidden = document.querySelector(".button_hidden");
   const buttonedition = document.createElement("div");
   buttonedition.innerHTML =
-    '<i class="fa-regular fa-pen-to-square"></i><p>Modifier</p>';
+    '<i class="fa-regular fa-pen-to-square"></i><p class="edit_modifier">Modifier</p>';
   buttonHidden.appendChild(buttonedition);
   // Ajoutez le code suivant pour masquer les boutons de filtrage en mode édition
   const filterButtons = document.getElementById("Filtres"); // Remplacez "filterButtons" par l'ID réel des boutons de filtrage
